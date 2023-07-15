30c0f5ee2a5db8aaa1c9b29ae58ec28f9e3084cd
Ivan Drahoner <111982515+idrahoner@users.noreply.github.com>
2023-07-15 13:55:10 +0300

    Add basePath to next.config.js

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,19 +3,19 @@
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 
-export default function Header() {
+export default function Footer() {
   const pathname = usePathname();
 
   return (
     <footer className="w-screen h-32 flex flex-wrap justify-center items-center gap-8 py-8 bg-sky-800">
-      {pathname !== '/distillery-calculator' && (
+      {/* {pathname !== '/' && (
         <Link
           href="/"
           className="text-orange-100 border-2 border-solid border-orange-100 rounded-lg px-4 py-2"
         >
           {'<-'} На гловну
         </Link>
-      )}
+      )} */}
       <p className="text-orange-100">&copy; copyright 2023</p>
     </footer>
   );
