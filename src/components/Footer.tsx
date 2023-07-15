82f51113c61c6c0ba2e9e1b836fc546c237c246c
Ivan Drahoner <111982515+idrahoner@users.noreply.github.com>
2023-07-15 20:28:51 +0300

    Removed some components

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,21 +1,21 @@
 'use client';
 
-import { usePathname } from 'next/navigation';
-import Link from 'next/link';
+// import { usePathname } from 'next/navigation';
+// import Link from 'next/link';
 
 export default function Footer() {
-  const pathname = usePathname();
+  // const pathname = usePathname();
 
   return (
     <footer className="w-screen h-32 flex flex-wrap justify-center items-center gap-8 py-8 bg-sky-800">
-      {pathname !== '/' && (
+      {/* {pathname !== '/' && (
         <Link
           href="/"
           className="text-orange-100 border-2 border-solid border-orange-100 rounded-lg px-4 py-2"
         >
           {'<-'} На гловну
         </Link>
-      )}
+      )} */}
       <p className="text-orange-100">&copy; copyright 2023</p>
     </footer>
   );
