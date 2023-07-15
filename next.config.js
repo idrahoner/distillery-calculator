30bfcc5d111e56397285b6d196ba2b44d243648f
Ivan Drahoner <111982515+idrahoner@users.noreply.github.com>
2023-07-15 16:13:29 +0300

    Return to initial stata and create not found page

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/distillery-calculator',
-  assetPrefix: '/distillery-calculator/',
 };
 
 module.exports = nextConfig;
