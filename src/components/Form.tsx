7367bc57d40d7398150ef594d53184f0c2a51304
Ivan Drahoner <111982515+idrahoner@users.noreply.github.com>
2023-07-16 14:47:54 +0300

    Refactored form to controlled inputs and add styles for inputs

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,38 +1,66 @@
 'use client';
-import { useState } from 'react';
+import { ChangeEvent, FormEvent, useState } from 'react';
+import FormField from './FormField';
+
+const FORM_INITIAL_STATE = { spirit: '', strength: '' };
 
 export default function Form() {
   const [result, setResult] = useState('');
+  const [formState, setFormState] = useState(FORM_INITIAL_STATE);
+
+  const handleInputCahnge = ({ target }: ChangeEvent<HTMLInputElement>) => {
+    setFormState((prevState) => ({
+      ...prevState,
+      [target.name]: target.value,
+    }));
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const essentialSpirit =
+      (Number(formState.strength) / 100) * Number(formState.spirit);
+
+    const heads = essentialSpirit * 0.11;
+    const tails = essentialSpirit * 0.17;
+    const body = essentialSpirit - heads - tails;
+
+    setResult(
+      `При перегоні буде отримано ${heads.toFixed(2)}л голів, ${body.toFixed(
+        2
+      )}л тіла та ${tails.toFixed(2)}л хвостів. Вдалого перегону :)`
+    );
+  };
+
+  const handleCancel = () => {
+    setResult('');
+    setFormState(FORM_INITIAL_STATE);
+  };
+
   return (
     <form
       name="calculator"
       className="flex flex-col gap-4 w-5/6"
-      onSubmit={(event) => {
-        event.preventDefault();
-        const { spirit, strength } = event.target as typeof event.target & {
-          spirit: { value: string };
-          strength: { value: string };
-        };
-
-        const essentialSpirit =
-          (Number(strength.value) / 100) * Number(spirit.value);
-
-        const heads = essentialSpirit * 0.11;
-        const tails = essentialSpirit * 0.17;
-        const body = essentialSpirit - heads - tails;
-
-        setResult(
-          `При перегоні буде отримано ${heads.toFixed(
-            2
-          )}л голів, ${body.toFixed(2)}л тіла та ${tails.toFixed(
-            2
-          )}л хвостів. Вдалого перегону :)`
-        );
-        event.currentTarget.reset();
-      }}
+      onSubmit={handleSubmit}
     >
-      <input type="number" name="spirit" placeholder="Введіть перше число" />
-      <input type="number" name="strength" placeholder="Введіть друге число" />
+      <FormField
+        label="Сирець (л):"
+        type="number"
+        name="spirit"
+        placeholder="Об‘єм сирцю в літрах"
+        value={formState.spirit}
+        onChange={handleInputCahnge}
+      />
+      <FormField
+        label="Міцність (%):"
+        type="number"
+        name="strength"
+        min={0}
+        max={100}
+        step={0.1}
+        placeholder="Міцність в відсотках"
+        value={formState.strength}
+        onChange={handleInputCahnge}
+      />
       <button type="submit">Порахувати</button>
       <button type="button" onClick={() => setResult('')}>
         Стерти
---
+++ src/components/FormField.tsx
@@ -0,0 +1,17 @@
+import { InputHTMLAttributes } from 'react';
+
+interface IProps extends InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+}
+
+export default function FormField({ label, ...rest }: IProps) {
+  return (
+    <label className="flex flex-col gap-2">
+      {label}
+      <input
+        {...rest}
+        className="px-2 py-1 border-2 border-solid border-sky-800 rounded-md focus:outline-none focus:shadow-md focus:border-sky-500 invalid:border-red-500"
+      />
+    </label>
+  );
+}
